cba68d7238ccda6ae573809a35874d8391c6882f
moleium <molenoch@protonmail.com>
2025-09-24 21:58:46 +0300

    chore: std print for dbg macro

--- src/util/logger.h
+++ src/util/logger.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <print>
+
 #ifdef DEBUG
 #include <cstdio>
 
-#define LOG(...) printf(__VA_ARGS__)
+#define LOG(...) std::print(__VA_ARGS__)
 #else
 #define LOG(...) 
 #endif
